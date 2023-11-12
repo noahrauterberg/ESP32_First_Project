7faaa6bd5eb21d098f0fff26bd14e1cc56086bd2
noah <rauterberg@campus.tu-berlin.de>
2023-11-12 17:53:52 +0100

    one bt service running

--- main/Einstiegsaufgabe.c
+++ main/Einstiegsaufgabe.c
@@ -6,18 +6,245 @@
 #include "freertos/event_groups.h"
 #include "esp_http_client.h"
 
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_gatt_common_api.h"
+#include "esp_gatt_defs.h"
+
 #define WIFI_READ_INFO BIT0
 #define WIFI_GOT_IP_BIT BIT1
 
 #define POST_URL "https://europe-west3-einstiegsaufgabe.cloudfunctions.net/receive_data"
 #define POST_PORT 80
 
+#define BLUETOOTH_NAME "esp32"
+#define GATTS_SERVICE_UUID 0xFF
+#define PREPARE_BUF_MAX_SIZE 1024
+
 static EventGroupHandle_t wifi_event_group;
 
 char wifi_ssid[32];
 char wifi_password[64];
 
-void http_event_handler(esp_http_client_event_handle_t event) {
+// COPY-PASTE examples/bluedroid/ble/gatt_server/main.c
+struct gatts_profile_inst {
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_handle;
+    esp_gatt_srvc_id_t service_id;
+    uint16_t char_handle;
+    esp_bt_uuid_t char_uuid;
+    esp_gatt_perm_t perm;
+    esp_gatt_char_prop_t property;
+    uint16_t descr_handle;
+    esp_bt_uuid_t descr_uuid;
+};
+static uint8_t adv_service_uuid128[32] = {
+    /* LSB <--------------------------------------------------------------------------------> MSB */
+    //first uuid, 16bit, [12],[13] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xEE, 0x00, 0x00, 0x00,
+    //second uuid, 32bit, [12], [13], [14], [15] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,
+};
+//adv data
+static esp_ble_adv_data_t adv_data = {
+    .set_scan_rsp = false,
+    .include_name = true,
+    .include_txpower = false,
+    .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
+    .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(adv_service_uuid128),
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+// scan response data
+static esp_ble_adv_data_t scan_rsp_data = {
+    .set_scan_rsp = true,
+    .include_name = true,
+    .include_txpower = true,
+    //.min_interval = 0x0006,
+    //.max_interval = 0x0010,
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(adv_service_uuid128),
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+static esp_ble_adv_params_t adv_params = {
+    .adv_int_min        = 0x20,
+    .adv_int_max        = 0x40,
+    .adv_type           = ADV_TYPE_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    //.peer_addr            =
+    //.peer_addr_type       =
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+typedef struct {
+    uint8_t                 *prepare_buf;
+    int                     prepare_len;
+} prepare_type_env_t;
+// End Copy-Paste
+
+static uint8_t attr_val[] = {0x11, 0x22, 0x33};
+static esp_attr_value_t service_char_value = {
+    .attr_max_len = 64,
+    .attr_value = attr_val,
+    .attr_len = sizeof(attr_val)
+};
+
+static struct gatts_profile_inst bt_profile = {};
+static prepare_type_env_t write_prep_env;
+
+void print_uint8_as_char(uint8_t* msg, uint16_t len) {
+    char str[len + 1];
+    str[len] = '\0';
+    memcpy(str, msg, len);
+    printf("got %s\n", str);
+}
+
+static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
+    switch (event) {
+        case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
+            esp_ble_gap_start_advertising(&adv_params);
+            break;
+        case ESP_GAP_BLE_SCAN_RSP_DATA_SET_COMPLETE_EVT:
+            // advertise the whole time (?)
+            esp_ble_gap_start_advertising(&adv_params);
+            break;
+        case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+            if (param -> adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+                printf("Some error occured during advertising start\n");
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param) {
+    switch (event) {
+        case ESP_GATTS_REG_EVT:
+            bt_profile.service_id.is_primary = true;
+            bt_profile.service_id.id.inst_id = 0x00;
+            bt_profile.service_id.id.uuid.len = ESP_UUID_LEN_16;
+            bt_profile.service_id.id.uuid.uuid.uuid16 = GATTS_SERVICE_UUID;
+
+            esp_ble_gap_set_device_name(BLUETOOTH_NAME);
+
+            esp_ble_gap_config_adv_data(&adv_data);
+            // ?
+            esp_ble_gap_config_adv_data(&scan_rsp_data);
+            esp_ble_gatts_create_service(gatts_if, &bt_profile.service_id, 4);
+            break;
+        case ESP_GATTS_CREATE_EVT:
+            bt_profile.service_handle = param -> create.service_handle;
+            bt_profile.char_uuid.len = ESP_UUID_LEN_16;
+            bt_profile.char_uuid.uuid.uuid16 = GATTS_SERVICE_UUID;
+
+            // start service
+            esp_ble_gatts_start_service(bt_profile.service_handle);
+            esp_gatt_char_prop_t service_properties = ESP_GATT_CHAR_PROP_BIT_WRITE;
+            esp_ble_gatts_add_char(bt_profile.service_handle, &bt_profile.char_uuid, ESP_GATT_PERM_WRITE, service_properties,&service_char_value, ESP_GATT_RSP_BY_APP);
+            esp_ble_gatts_add_char_descr(bt_profile.service_handle, &bt_profile.descr_uuid, ESP_GATT_PERM_WRITE, NULL, NULL);
+            break;
+        case ESP_GATTS_ADD_CHAR_EVT:
+            bt_profile.char_handle = param -> add_char.attr_handle;
+            bt_profile.descr_uuid.len = ESP_UUID_LEN_16;
+            bt_profile.descr_uuid.uuid.uuid16 = ESP_GATT_UUID_CHAR_CLIENT_CONFIG;
+            
+            int char_len = 0;
+            uint8_t* char_val;
+            esp_ble_gatts_get_attr_value(bt_profile.char_handle, &char_len, &char_val);
+
+            break;
+        case ESP_GATTS_CONNECT_EVT:
+            printf("Device connected \n");
+            esp_ble_conn_update_params_t conn_params = {0};
+            memcpy(conn_params.bda, param->connect.remote_bda, sizeof(esp_bd_addr_t));
+            // values copied from example
+            conn_params.latency = 0;
+            conn_params.min_int = 0x10;
+            conn_params.max_int = 0x20;
+            conn_params.timeout = 400;
+            bt_profile.conn_id = param -> connect.conn_id;
+            esp_ble_gap_update_conn_params(&conn_params);
+            break;
+        case ESP_GATTS_DISCONNECT_EVT:
+            printf("Device disconnected \n");
+            // need to advertise once again
+            esp_ble_gap_start_advertising(&adv_params);
+            break;
+        case ESP_GATTS_WRITE_EVT:
+            print_uint8_as_char(param->write.value, param->write.len);
+            if (!param->write.need_rsp) {
+                // if no response i needed, we do not respond
+                printf("No response needed\n");
+                // converting uint8_t to str/char[]
+                char msg[param->write.len + 1];
+                msg[param->write.len] = '\0';
+                memcpy(msg, param->write.value, param->write.len);
+                printf("got %s\n", msg);
+                break;
+            }
+            // Client needs a response
+            printf("Response needed\n");
+            if (!param->write.is_prep) {
+                /* only need to handle write prep seperately, this doubles one line of code, 
+                * but keeps it unnested*/
+                esp_ble_gatts_send_response(gatts_if, param->write.conn_id, param->write.trans_id, ESP_GATT_OK, NULL);
+                break;
+            }
+
+            esp_gatt_status_t status = ESP_GATT_OK;
+            if (write_prep_env.prepare_buf == NULL) {
+                // no need to free prepare_buf (?), as it needs to be used the whole time
+                write_prep_env.prepare_buf = (uint8_t*) malloc(PREPARE_BUF_MAX_SIZE*sizeof(uint8_t));
+                // yes, you should check prepafe_buf == NULL
+                write_prep_env.prepare_len = 0;
+            } else {
+                if((param->write.offset + param->write.len) > PREPARE_BUF_MAX_SIZE) {
+                    // this is not 100% correct, but good enough
+                    status = ESP_GATT_INVALID_ATTR_LEN;
+                }
+            }
+
+            esp_gatt_rsp_t* gatt_rsp = (esp_gatt_rsp_t *)malloc(sizeof(esp_gatt_rsp_t));
+            // again - you should check whether gatt_rsp == NULL
+
+            // create response
+            memcpy(gatt_rsp->attr_value.value, param->write.value, param->write.len);
+            gatt_rsp -> attr_value.handle = param -> write.handle;
+            gatt_rsp -> attr_value.offset = param -> write.offset;
+            gatt_rsp -> attr_value.len = param -> write.len;
+            gatt_rsp -> attr_value.auth_req = ESP_GATT_AUTH_REQ_NONE;
+            
+            esp_ble_gatts_send_response(gatts_if, param->write.conn_id, param->write.trans_id, status, gatt_rsp);
+            free(gatt_rsp);
+
+            // copy data into buffer
+            memcpy(write_prep_env.prepare_buf+param->write.offset, param->write.value, param->write.len);
+            write_prep_env.prepare_len += param->write.len;
+            break;
+        case ESP_GATTS_EXEC_WRITE_EVT:
+            printf("exec write event\n");
+            break;
+        default:
+            break;
+    }
+}
+
+static void http_event_handler(esp_http_client_event_handle_t event) {
     switch (event->event_id) {
         case HTTP_EVENT_ON_CONNECTED:
             printf("Successfully connected\n");
@@ -33,7 +260,7 @@ void http_event_handler(esp_http_client_event_handle_t event) {
     }
 }
 
-void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     switch (event_id){
         case WIFI_EVENT_STA_START:
             printf("Started wifi\n");
@@ -63,8 +290,7 @@ void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, in
     }
 }
 
-/* Establishes a WIFI-Connection
-*/
+// Establishe a WIFI-Connection
 void connect_to_wifi() {
     // Initialization Phase
     esp_err_t err = esp_netif_init();
@@ -118,6 +344,7 @@ void get_http() {
 }
 */
 
+// Post HTTP request to POST_URL and initialize http_event_handler
 void post_http() {
     printf("posting http\n");
     esp_http_client_config_t http_config = {
@@ -144,8 +371,9 @@ void post_http() {
     esp_http_client_cleanup(client);
 }
 
-// reading wifi ssid and password
+// Read wifi ssid and password from nvs
 void read_wifi() {
+    printf("reading wifi\n");
     nvs_handle_t wifi_handle;
     esp_err_t err = nvs_open("wifi_storage", NVS_READWRITE, &wifi_handle);
     if (err != ESP_OK) {
@@ -177,6 +405,7 @@ void read_wifi() {
     nvs_close(wifi_handle);
 }
 
+// Write wifi ssid and password to nvs
 void write_wifi(char* ssid, char* password) {
     nvs_handle_t wifi_handle;
     esp_err_t err = nvs_open("wifi_storage", NVS_READWRITE, &wifi_handle);
@@ -202,6 +431,34 @@ void write_wifi(char* ssid, char* password) {
     nvs_close(wifi_handle);
 }
 
+// Start the bluetooth module and advertise
+void start_bluetooth() {
+    printf("starting bluetooth\n");
+    esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT);
+    esp_bt_controller_config_t config = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    esp_err_t err = esp_bt_controller_init(&config);
+    if (err != ESP_OK) {
+        printf("failure\n");
+        return;
+    }
+    err = esp_bt_controller_enable(ESP_BT_MODE_BLE);
+
+    // enable bluedroid
+    esp_bluedroid_config_t bluedroid_cfg = BT_BLUEDROID_INIT_CONFIG_DEFAULT();
+    err = esp_bluedroid_init_with_cfg(&bluedroid_cfg);
+    err = esp_bluedroid_enable();
+
+    // event handler
+    err = esp_ble_gatts_register_callback(gatts_event_handler);
+    err = esp_ble_gap_register_callback(gap_event_handler);
+
+    // Application Profile(s)
+    esp_ble_gatts_app_register(0);
+
+    // ?
+    esp_ble_gatt_set_local_mtu(500);
+}
+
 void app_main(void) {
     // copy-paste from station_example_main.c
     // Initialize NVS
@@ -212,7 +469,10 @@ void app_main(void) {
     }
     ESP_ERROR_CHECK(err);
 
+    start_bluetooth();
+
     wifi_event_group = xEventGroupCreate();
+    /*
     read_wifi();
 
     EventBits_t wifi_bits = xEventGroupGetBits(wifi_event_group);
@@ -229,6 +489,7 @@ void app_main(void) {
     
     xEventGroupWaitBits(wifi_event_group, WIFI_GOT_IP_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
     post_http();
+    */
 
     printf("finished\n");
 }
